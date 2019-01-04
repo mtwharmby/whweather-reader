e408f873bc744b9bd073e6d17e2505825adcaa31
Michael Wharmby <mtwharmby@gmail.com>
2019-01-04 15:49:02 +0000

    Completes transform method (passes test) and adds repr method to Reading class (with test)

--- test/test_whreader.py
+++ test/test_whreader.py
@@ -3,7 +3,14 @@ from whreader.whreader import transform_reading, Reading
 
 def test_transform():
     sensor_reading = "name=boris::time=2134457::temp=25.5::humid=56.8"
-    expected = Reading('boris', 2134457, readings={'temp': 25.5, 'humid': 56.8})
+    expected = Reading('boris', 2134457.0, readings={'temp': 25.5, 'humid': 56.8})
 
     structured_read = transform_reading(sensor_reading)
     assert structured_read == expected
+
+
+def test_Reading_repr():
+    expected = 'device_name=boris\nreading_time=2134457\n{}'.format({'temp': 25.5, 'humid': 56.8}.__repr__())
+
+    string_form = Reading('boris', 2134457, readings={'temp': 25.5, 'humid': 56.8}).__repr__()
+    assert string_form == expected
--- whreader/whreader.py
+++ whreader/whreader.py
@@ -1,8 +1,33 @@
 def transform_reading(reading):
-    pass
+    data_blocks = reading.split('::')
+    device_name = None
+    reading_time = None
+    readings = {}
+    for block in data_blocks:
+        key, value = block.split('=')
+        if key == 'name':
+            device_name = value
+        elif key == 'time':
+            # TODO Add checking whether device supplying reading is registered and then applying any needed offset
+            reading_time = float(value)
+        else:
+            readings[key] = float(value)
+
+    return Reading(device_name, reading_time, readings)
 
 
-class Reading():
+class Reading(object):
 
     def __init__(self, name, time, readings={}):
-        pass
+        self.device_name = name
+        self.reading_time = time
+        self.readings = readings
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __repr__(self):
+        return 'device_name={}\nreading_time={}\n'.format(self.device_name, self.reading_time) + self.readings.__repr__()
