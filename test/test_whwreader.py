b7e3d2a7cda380af39115bcfd1fcdf9c58f78c3c
Michael Wharmby <mtwharmby@gmail.com>
2019-01-06 20:02:49 +0000

    Implements basic device registration

--- test/test_whwreader.py
+++ test/test_whwreader.py
@@ -14,7 +14,7 @@ def test_transform():
 
 def test_device_registration():
     test_sensor_rpi1 = SensorDevice('RPi1')
-    test_sensor_ard = SensorDevice('arduino1', abs_time=False)
+    test_sensor_ard = SensorDevice('arduino1', time_offset=1546804623.6360931)
 
     assert len(whwreader.__sensor_devices) == 0
     whwreader.register_sensor(test_sensor_rpi1)
--- whwreader/whwreader.py
+++ whwreader/whwreader.py
@@ -1,3 +1,6 @@
+__sensor_devices = {}
+
+
 def transform_reading(reading):
     data_blocks = reading.split('::')
     device_name = None
@@ -16,10 +19,17 @@ def transform_reading(reading):
     return Reading(device_name, reading_time, readings)
 
 
+def register_sensor(device):
+    id = device.id
+    if id in __sensor_devices.keys():
+        raise Exception('Device with ID {} already registered'.format(id))
+    __sensor_devices[id] = device
+
+
 class Reading(object):
 
-    def __init__(self, name, time, readings={}):
-        self.device_name = name
+    def __init__(self, dev_id, time, readings={}):
+        self.device_id = dev_id
         self.reading_time = time
         self.readings = readings
 
@@ -30,4 +40,20 @@ class Reading(object):
             return False
 
     def __repr__(self):
-        return 'device_name={}\nreading_time={}\n'.format(self.device_name, self.reading_time) + self.readings.__repr__()
+        return 'device_id={}\nreading_time={}\n'.format(self.device_id, self.reading_time) + self.readings.__repr__()
+
+
+class SensorDevice(object):
+
+    def __init__(self, id, time_offset=0):
+        self.id = id
+        self.time_offset = time_offset
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __repr__(self):
+        return 'device_id={0}\ntime_offset={1}'.format(self.id, self.time_offset)
