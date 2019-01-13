99db25db70471e88b0fa3f6eac4650db05b2be11
Michael Wharmby <mtwharmby@gmail.com>
2019-01-13 17:42:52 +0000

    Simplified method for setting time offset

--- test/test_whwreader.py
+++ test/test_whwreader.py
@@ -1,33 +1,22 @@
 import pytest
 
 import whwreader.whwreader as whwreader
-from whwreader.whwreader import Reading, SensorDevice
+from whwreader.whwreader import Reading
 
 
 def test_transform():
-    sensor_reading = "name=boris::time=2134457::temp=25.5::humid=56.8"
-    expected = Reading('boris', 2134457.0, readings={'temp': 25.5, 'humid': 56.8})
+    #Preamble
+    whwreader.__sensor_time_offset['boris'] = 0
+    whwreader.__sensor_time_offset['charles'] = 1546804623.6360931
 
+    sensor_reading = "name=boris::time=2134457::temp=25.5::humid=56.8"
+    #expected = Reading('boris', 2134.457, readings={'temp': 25.5, 'humid': 56.8}, sensor_unknown=False)
+    expected = Reading('boris', 2134.457, {'temp': 25.5, 'humid': 56.8}, sensor_unknown=False)
     structured_read = whwreader.transform_reading(sensor_reading)
     assert structured_read == expected
 
-    #TODO Add test of updating time
-
-
-def test_device_registration():
-    test_sensor_rpi1 = SensorDevice('RPi1')
-    test_sensor_ard1 = SensorDevice('arduino1', time_offset=1546804623.6360931)
-
-    assert len(whwreader.__sensor_devices) == 0
-    whwreader.register_sensor(test_sensor_rpi1)
-    assert len(whwreader.__sensor_devices) == 1
-    assert whwreader.__sensor_devices['RPi1'] == test_sensor_rpi1
-
-    with pytest.raises(Exception):
-        #Shouldn't be able to register same sensor (ID) twice
-        whwreader.register_sensor(test_sensor_rpi1)
-
-    whwreader.register_sensor(test_sensor_ard1)
-    assert len(whwreader.__sensor_devices) == 2
-    assert whwreader.__sensor_devices['arduino1'] == test_sensor_ard1
-    
+    #Now test against an offset time. N.B. Expecting time in ms
+    sensor_reading_2 = "name=charles::time=600::temp=32.5::humid=22.6"
+    expected_2 = Reading('charles', 1546804623.6360931 + 0.6, readings={'temp': 32.5, 'humid': 22.6})
+    structured_read = whwreader.transform_reading(sensor_reading_2)
+    assert structured_read == expected_2
--- whwreader/whwreader.py
+++ whwreader/whwreader.py
@@ -1,35 +1,30 @@
-__sensor_devices = {}
-
+__sensor_time_offset = {}
 
 def transform_reading(reading):
     data_blocks = reading.split('::')
-    device_name = None
+    sensor_id = None
+    sensor_unknown = False
     reading_time = None
     readings = {}
     for block in data_blocks:
         key, value = block.split('=')
         if key == 'name':
-            device_name = value
+            if value not in __sensor_time_offset.keys():
+                sensor_unknown = True
+            sensor_id = value
         elif key == 'time':
-            # TODO Add checking whether device supplying reading is registered and then applying any needed offset
-            reading_time = float(value)
+            reading_time = float(value)/1000 + __sensor_time_offset[sensor_id]
         else:
             readings[key] = float(value)
 
-    return Reading(device_name, reading_time, readings)
-
-
-def register_sensor(device):
-    id = device.id
-    if id in __sensor_devices.keys():
-        raise Exception('Device with ID {} already registered'.format(id))
-    __sensor_devices[id] = device
+    return Reading(sensor_id, reading_time, readings = readings, sensor_unknown = sensor_unknown)
 
 
 class Reading(object):
 
-    def __init__(self, dev_id, time, readings={}):
-        self.device_id = dev_id
+    def __init__(self, sensor_id, time, readings={}, sensor_unknown = False):
+        self.sensor_id = sensor_id
+        self.sensor_unknown = sensor_unknown
         self.reading_time = time
         self.readings = readings
 
@@ -40,20 +35,4 @@ class Reading(object):
             return False
 
     def __repr__(self):
-        return 'device_id={}\nreading_time={}\n'.format(self.device_id, self.reading_time) + self.readings.__repr__()
-
-
-class SensorDevice(object):
-
-    def __init__(self, id, time_offset=0):
-        self.id = id
-        self.time_offset = time_offset
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        else:
-            return False
-
-    def __repr__(self):
-        return 'device_id={0}\ntime_offset={1}'.format(self.id, self.time_offset)
+        return 'sensor_id={0}\nsensor_unknown={1}\nreading_time={2}\n'.format(self.sensor_id, self.sensor_unknown, self.reading_time) + self.readings.__repr__()
